2558e39e36e8db3a67fba5b9185b17e0f9175774
JennyALim <jenny.analim@gmail.com>
2025-11-30 18:11:38 +0000

    First commit: Portfolio website + Flask app

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,52 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-comments = []
+# --------------------------------------------------------------------
+#  DATABASE CONFIG  (Replace YOURPASSWORD with your MySQL password)
+# --------------------------------------------------------------------
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+mysqlconnector://JennyALim:jenny12345@JennyALim.mysql.pythonanywhere-services.com/JennyALim$mydatabase"
+)
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
+
+#  DATABASE
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096), nullable=False)
+
+
+#  ROUTES
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        all_comments = Comment.query.all()
+        return render_template("main_page.html", comments=all_comments)
 
-    comments.append(request.form["contents"])
-    return redirect(url_for('index'))
+    # POST: add new comment
+    new_comment = Comment(content=request.form["contents"])
+    db.session.add(new_comment)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+#  DELETE COMMENT
+@app.route("/delete/<int:comment_id>", methods=["POST"])
+def delete_comment(comment_id):
+    comment = Comment.query.get(comment_id)
+    if comment:
+        db.session.delete(comment)
+        db.session.commit()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run()
